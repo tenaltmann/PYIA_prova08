c743c251c2c09dad6cef5a3041a9f13c8f93fd70
Ricardo Altmann <tenaltmann@gmail.com>
2025-04-22 09:25:39 -0300

    Adicionando comentarios sobre o que foi feito no arquivo para gerar o resultado pedido em atividade

--- PYIA_prova08.py
+++ PYIA_prova08.py
@@ -1,6 +1,6 @@
-import os
+import os                   # importamos o modulo OS
 
-print(os.listdir())
+print(os.listdir())         # printamos os diretórios existentes com o os.listdir()
 
-for i in os.listdir():
+for i in os.listdir():      # usamos o for para iterar sobre os diretórios e exibi-los individualmente no console
     print(i)
